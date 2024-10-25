b1ce92e9dacb2f8e4e3ff8a638654f2050d7a2da
IltoMartinsNavesNeto <iltoneto400@gmail.com>
2024-10-25 11:47:47 -0300

    Começando a implantar Board e Piece

--- chessSystem/src/application/Program.java
+++ chessSystem/src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
-import boardgame.Positon;
+import boardgame.Board;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Positon pos = new Positon(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8, 8);
 		
 	}
 
